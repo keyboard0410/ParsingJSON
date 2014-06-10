03aff607eeffb23017cb0319a6d1b7c96f03186f
Gang Liu <liugang.zzq@gmail.com>
2014-06-10 02:31:06 -0700

    basic frame works, need some details work

--- __init__.py
+++ __init__.py
@@ -5,94 +5,99 @@ from pprint import pprint
 from lxml import etree
 from lxml.html.clean import clean_html
 import re
+from HTMLParser import HTMLParser
+from htmlentitydefs import name2codepoint
 
-#def generatePricingInfo():
+
+class Node():
+    def __init__(self):
+        self.tag_name =''
+        self.data_value=''
+    
+class MyHTMLParser(HTMLParser):
+ 
+    
+    list=[]
+    i =0
+    def handle_starttag(self, tag, attrs):
+        if(self.i==0):
+            self.list=[];
+            self.i=1
+        mynode=Node();
+        #print "Encountered a start tag:", tag
+        mynode.tag_name  = 'startTag'
+        mynode.data_value = tag
+        self.list.append(mynode)
+        
+    def handle_data(self, data):
+        #print "Encountered some data  :", data
+        mynode=Node();
+        mynode.tag_name  = 'data'
+        mynode.data_value = data
+        self.list.append(mynode)
+        
+    def handle_endtag(self, tag):
+        #print "Encountered an end tag :", tag
+        mynode=Node();
+        mynode.tag_name  = 'endTag'
+        mynode.data_value = tag
+        self.list.append(mynode)
+        
+
+
+
 with gzip.open("I:\gangLiu\everstring\pricing_data_set.json", "r") as infile:
     pricingInformation = []
-    try:
-        for i, line in enumerate(infile):
-            data = json.loads(line.strip())
-            #pprint (data)
-            if(data.get('html') != ""):
-                htmlRaw = data.get('html')
-            #print "Now processing this website ######################################################################################"
-            #print data.get('link_url')
-                print i
-                try:
-                    htmlTree = etree.HTML(htmlRaw)
-                except ValueError:
-                    print "ValueError!!!! Need XMLParser to parse XML file"
-                    pass
-                result = etree.tostring(htmlTree, pretty_print=True, method="html")
-                html_Cleaned = clean_html(result)
-            else:
-                pass
-            eachDict = {}
-            eachDict['link_url'] = data.get('link_url')
+    for i, line in enumerate(infile):
+        parser = MyHTMLParser()
+        data = json.loads(line.strip())
+        #pprint (data)
+        if(data.get('html') != ""):
+            parser.feed(data.get('html'))
+            #print i
             
-            if(data['link_text'].lower().find('pricing') != -1 or data['link_text'].lower().find('rates') != -1):
-                startIndexOfValue = html_Cleaned.find('$')
-                if(startIndexOfValue != -1):
-                    endIndexOfValue = startIndexOfValue + 1
-                    while(endIndexOfValue < len(html_Cleaned)):
-                        if(html_Cleaned[endIndexOfValue] >= '0' and html_Cleaned[endIndexOfValue] <= '9'):
-                            endIndexOfValue = endIndexOfValue + 1
-                        elif(html_Cleaned[endIndexOfValue] == '.'):
-                            endIndexOfValue = endIndexOfValue + 1
-                        elif(html_Cleaned[endIndexOfValue] == ','):
-                            endIndexOfValue = endIndexOfValue + 1
-                        elif(html_Cleaned[endIndexOfValue] == '+'):
-                            endIndexOfValue = endIndexOfValue + 1
-                        else:
-                            break
-                    value = html_Cleaned[startIndexOfValue:endIndexOfValue]
-                if(html_Cleaned[startIndexOfValue-1] == '>' and html_Cleaned[endIndexOfValue] == '<'):
-                    tagsEndOfField = ['</strong>', '</span>', '</font>', '</div>', '</h1>', '</h2>', '</h3>', '</h4>']
-                    for tag in tagsEndOfField:
-                        startIndexOfTag = html_Cleaned.rfind(tag, 0, startIndexOfValue-1)
-                        if (startIndexOfTag != -1):
-                            startIndexOfField = startIndexOfTag - 1
-                            indexOfTagPair = html_Cleaned.rfind('>', 0, startIndexOfTag)
-                            strTemp = html_Cleaned[indexOfTagPair+1:startIndexOfTag]
-                            field1 = re.search('>[\w\s*]+<', strTemp)
-                            try:
-                                field = field1.group(0)[1:-1]
-                                eachDict[field] = value
-                                print eachDict
-                            except AttributeError:
-                                pass 
-                        ###########modify   
-                            
-                elif(html_Cleaned[startIndexOfValue-1] != '>'):
-                    startIndexOfField = startIndexOfValue-1
-                    while(html_Cleaned[startIndexOfField] != '>'):
-                        startIndexOfField = startIndexOfField - 1
-                    startIndexOfField = startIndexOfField + 1
-                    field = html_Cleaned[startIndexOfField:startIndexOfValue]
-                    eachDict[field] = value
-                    print eachDict
+            sList = parser.list
+            index = 0
+            newString = ''
+            
+            ###process each html data in sList
+            ##case1: before $*** there are some words; the words is the service name;
+            
+            
+            ##case2: no words beofore $****; search for the previous line. until 1)there is only one data within startTag and endTag and 2)length of the data is less than some value 3) there is no prince in this data eg: 30 note: 1) 2)and 3) all true;  
+            ##      
+            
+            
+            while(index < len(sList)):
+                if(sList[index].tag_name == 'data' and sList[index].data_value.find('$')!=-1):
                     
-                elif(html_Cleaned[endIndexOfValue] != '<'):
-                    endIndexOfField = endIndexOfValue
-                    while(html_Cleaned[endIndexOfField] != '<'):
-                        endIndexOfField = endIndexOfField + 1
-                    field = html_Cleaned[endIndexOfValue:endIndexOfField]
-                    eachDict[field] = value
-                    print eachDict
+                    pos_dollar = sList[index].data_value.find('$')
+                    if(sList[index].data_value[pos_dollar+1].isdigit() and pos_dollar > 0 ):
+                        print "iterm:" + sList[index].data_value[0:pos_dollar]
+                        
+                        
+                        print "price:" + sList[index].data_value[pos_dollar:len(sList[index].data_value)-1]
                     
-            else:
-                eachDict['product/service/solution'] = 'Need a quote to get pricinginfo'   
-            #print eachDict
-            pricingInformation.append(eachDict)
-            raw_input("enter to continue")
-    except IOError:
-        print "Error: cannot find file or read data"
+                    
+                
+                index = index + 1
+                
+                
+                
+        else:
+            pass
+        
+        parser.i=0
+        del parser
     
+        raw_input("----------------------enter to continue")
+    
+    
+    #print newString
+ 
         
-        
-        #print pricingInformation
             
-        #return pricingInformation
         
         
         
+                                    
